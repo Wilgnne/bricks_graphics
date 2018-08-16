81f47e19c106b549ab7cf4a5d53101c8c4901716
Wilgnne K <w.khawanbarbosa@gmail.com>
2018-08-16 14:37:59 -0300

    Car games complite

--- bricks_toolkit/screen_controller.py
+++ bricks_toolkit/screen_controller.py
@@ -11,7 +11,7 @@ class BricksWall():
 		self.itam = itam
 		self.off = off
 		self.on = on
-		width = self.j * (self.tam + self.esp) + 200
+		width = self.j * (self.tam + self.esp) + 105
 		height = self.i * (self.tam + self.esp) + 6
 		self.win = GraphWin('Briks', width, height, autoflush=False)
 		self.win.setBackground(color_rgb(217, 217, 243))
@@ -40,16 +40,33 @@ class BricksWall():
 		b.setWidth(2)
 		b.setOutline(color_rgb(255, 165, 0))
 		b.draw(self.win)
+		self.auxWall = []
+		for i in range(0, 4):
+			line = []
+			for j in range(0, 4):
+				rec = Rectangle(Point(0, 0), Point(self.tam, self.tam))
+				irec = Rectangle(Point(self.itam, self.itam), Point(self.tam - self.itam, self.tam - self.itam))
+				rec.setOutline(self.off)
+				irec.setFill(self.off)
+				irec.setOutline(self.off)
+				dx = (j * self.tam) + (j * self.esp) + self.esp + self.wall[0][self.j - 1][0].getP2().x + 10
+				dy = (i * self.tam) + (i * self.esp) + self.esp + self.wall[int(self.i / 2) - 2][0][0].getP2().y
+				rec.move(dx, dy)
+				irec.move(dx, dy)
+				rec.draw(self.win)
+				irec.draw(self.win)
+				line.append([rec, irec])
+			self.auxWall.append(line)
 
-	def clean(self):
-		for i in range(0, self.i):
-			for j in range(0, self.j):
-				self.wall[i][j][0].setOutline(self.off)
-				self.wall[i][j][1].setFill(self.off)
-				self.wall[i][j][1].setOutline(self.off)
+	def clean(self, wall):
+		for i in range(0, len(wall)):
+			for j in range(0, len(wall[0])):
+				wall[i][j][0].setOutline(self.off)
+				wall[i][j][1].setFill(self.off)
+				wall[i][j][1].setOutline(self.off)
 
 	def overlap(self, objects):
-		self.clean()
+		self.clean(self.wall)
 		for gameObject in objects:
 			for i in range(0, len(gameObject.mesh)):
 				for j in range(0, len(gameObject.mesh[0])):
@@ -59,4 +76,57 @@ class BricksWall():
 						self.wall[y][x][0].setOutline(self.on)
 						self.wall[y][x][1].setOutline(self.on)
 						self.wall[y][x][1].setFill(self.on)
-		self.win.redraw()
+		#self.win.redraw()
+
+	def pointWall(self, wall):
+		self.clean(self.auxWall)
+		for i in range(0, 4):
+			for j in range(0,4):
+				if wall[i][j] == '1':
+					self.auxWall[i][j][0].setOutline(self.on)
+					self.auxWall[i][j][1].setOutline(self.on)
+					self.auxWall[i][j][1].setFill(self.on)
+
+def lifeBricks(life):
+	if life == 4:
+		return [['1', '0', '0', '0'],
+		 		['1', '0', '0', '0'],
+		 		['1', '0', '0', '0'],
+		 		['1', '0', '0', '0']]
+	elif life == 3:
+		return [['0', '0', '0', '0'],
+		 		['1', '0', '0', '0'],
+		 		['1', '0', '0', '0'],
+		 		['1', '0', '0', '0']]
+	elif life == 2:
+		return [['0', '0', '0', '0'],
+		 		['0', '0', '0', '0'],
+		 		['1', '0', '0', '0'],
+		 		['1', '0', '0', '0']]
+	elif life == 1:
+		return [['0', '0', '0', '0'],
+		 		['0', '0', '0', '0'],
+		 		['0', '0', '0', '0'],
+		 		['1', '0', '0', '0']]
+	elif life < 1 or life > 4:
+		return [['0', '0', '0', '0'],
+		 		['0', '0', '0', '0'],
+		 		['0', '0', '0', '0'],
+		 		['0', '0', '0', '0']]
+
+def dieBricks(frame):
+	if frame == 0:
+		return [['1', '0', '0', '1'],
+		 		['0', '1', '1', '0'],
+		 		['0', '1', '1', '0'],
+		 		['1', '0', '0', '1']]
+	elif frame == 1:
+		return [['0', '1', '1', '0'],
+		 		['1', '0', '0', '1'],
+		 		['1', '0', '0', '1'],
+		 		['0', '1', '1', '0']]
+	elif frame == 2:
+		return [['1', '0', '0', '1'],
+		 		['0', '1', '1', '0'],
+		 		['0', '1', '1', '0'],
+		 		['1', '0', '0', '1']]
--- main.py
+++ main.py
@@ -1,41 +1,91 @@
 from bricks_toolkit.screen_controller import *
 from bricks_toolkit.bricks_behaviour import *
+import random
+
+def die(life, p, road, cars, bricks):
+    print('morte')
+    life = life - 1
+    for cont in range(0, 2):
+        anim = GameObject(p.position)
+        anim.setMesh(dieBricks(cont))
+        bricks.overlap(road + cars + [anim])
+        bricks.win.redraw()
+        time.sleep(0.2)
+    return life
 
 wall = BricksWall()
 
 meshCar = [['0', '1', '0'], ['1', '1', '1'], ['0', '1', '0'], ['1', '0', '1']]
-player = GameObject(Vector2(3, 16))
+carList = []
+player = GameObject(Vector2(2, 16))
 player.setMesh(meshCar)
 
 road = [['1', '0', '0', '0', '0', '0', '0', '0', '0', '0'], 
         ['1', '0', '0', '0', '0', '0', '0', '0', '0', '1'], 
         ['0', '0', '0', '0', '0', '0', '0', '0', '0', '1']]
 roadList = []
+cont = 0
+lifes = 4
 
 for x in range(0,8):
     r = GameObject(Vector2(0, (x*3) - 3))
     r.setMesh(road)
     roadList.append(r)
+speed = 0
 
-framerate = 1.0/10
+framerate = 1.0/60
 now = time.time()
 nextFramerate = now + framerate
-while True:
+t= 0
+f = 0
+deltatime = framerate 
+while lifes > 0:
+    t = time.time()
     key = wall.win.checkKey()
+    if speed > 0:
+        if cont == 8:
+            rand = random.randint(0, 2)
+            if rand == 1:
+                car = GameObject(Vector2(2, -4))
+                car.setMesh(meshCar)
+                carList.append(car)
+            if rand == 2:
+                car = GameObject(Vector2(5, -4))
+                car.setMesh(meshCar)
+                carList.append(car)
+            cont = 0
+        else:
+            cont += 1
+
+        for elem in roadList:
+            elem.position.translate(Vector2(0, 1))
+            if elem.position.y > 19:
+                elem.position.y = -4
+
+        for elem in carList:
+            elem.position.translate(Vector2(0, 1))
+            if player.position.y - elem.position.y == 3 and player.position.x == elem.position.x:
+                lifes = die(lifes, player, roadList, carList, wall)
+                carList.remove(elem)
+            if elem.position.y > 19:
+                carList.remove(elem)
+        speed = 0
+    else:
+        speed += deltatime;
+
 
     if key ==  'd' and player.position.x < 6:
-        player.position.translate(Vector2(1, 0))
+        player.position = Vector2(5, 16)
     if key ==  'a' and player.position.x > 1:
-        player.position.translate(Vector2(-1, 0))
-    for elem in roadList:
-        print(elem.position)
-        elem.position.translate(Vector2(0, 1))
-        if elem.position.y > 19:
-            elem.position.y = -4
+        player.position = Vector2(2, 16)
 
-    wall.overlap(roadList + [player])
+    wall.overlap(roadList + carList + [player])
+    wall.pointWall(lifeBricks(lifes))
 
     while now < nextFramerate:
         time.sleep(nextFramerate - now)
         now = time.time()
     nextFramerate += framerate
+    f = time.time()
+    deltatime = f - t
+    print(1.0/deltatime)
